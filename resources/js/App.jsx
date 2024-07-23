9c36f53d9daf1e69764a857bd1fd5d2f3efa76cc
Adam <adam5596@stud.kea.dk>
2024-07-23 13:42:26 +0200

    simple frontend with fetching posts

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -2,19 +2,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+    const [posts, setPosts] = useState([]);
+
     useEffect(() => {
         getPosts();
     }, []);
 
     async function getPosts() {
-        await axios
-            .get("http://boarder.test/posts")
-            .then((response) => console.log(response.data));
+        try {
+            const response = await axios.get("http://boarder.test/posts");
+            setPosts(response.data);
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
     }
 
     return (
-        <div style={{ background: "red" }}>
-            <h1>Hello, react!</h1>
+        <div>
+            <h1>Boarder</h1>
+            <h2>For when youre bored on the board</h2>
+            <div className="container">
+                <div className="posts-container">
+                    {posts.map((post) => (
+                        <div key={post.id} className="post">
+                            <h3>{post.title}</h3>
+                            <p>{post.content}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "../css/app.css";
 
 createRoot(document.getElementById("app")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
